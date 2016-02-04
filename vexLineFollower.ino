0af864551078d3953bc8fb3b5697a46f15ec2416
michardy <mhardy17@bayschoolsf.org>
2016-02-04 13:10:52 -0800

    Added motor control

--- vexLineFollower.ino
+++ vexLineFollower.ino
@@ -1,9 +1,15 @@
+#include <Servo.h>
 #define cutoff 700
 int l, r, m;
+
+Servo servR;
+Servo servL;
 
 void setup() {
   Serial.begin(9600);
   pinMode(3, OUTPUT);
+  servR.attach(3);
+  servL.attach(5);
 }
 
 void loop() {
@@ -13,11 +19,19 @@ void loop() {
   //Serial.println(String(l) + ", " + String(m) + ", " + String(r));
   if (l > cutoff && !(m > cutoff) && !(r > cutoff)){
    Serial.println(F("Right")); 
+   servL.write(10);
+   servR.write(90);
   } else if (!(l > cutoff) && !(m > cutoff) && r > cutoff){
    Serial.println(F("Left"));
+   servR.write(10);
+   servL.write(90);
   } else if (m > cutoff){
    Serial.println(F("Middle"));
+   servR.write(10);
+   servL.write(10);
   } else if (l > cutoff && !(m > cutoff) && r > cutoff){
    Serial.println(F("Fork"));
+   servL.write(10);
+   servR.write(90);
   }
 }
