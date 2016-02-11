47b31948d62fb3fa26fac7aa98506a1f3c2af66d
michardy <mhardy17@bayschoolsf.org>
2016-02-11 10:41:51 -0800

    Ajusted settings for robot

--- vexLineFollower.ino
+++ vexLineFollower.ino
@@ -1,5 +1,10 @@
 #include <Servo.h>
 #define cutoff 700
+#define lowR 70
+#define lowL 110
+#define topR 0
+#define topL 180
+
 int l, r, m;
 
 Servo servR;
@@ -8,8 +13,8 @@ Servo servL;
 void setup() {
   Serial.begin(9600);
   pinMode(3, OUTPUT);
-  servR.attach(3);
-  servL.attach(5);
+  servR.attach(10);
+  servL.attach(11);
 }
 
 void loop() {
@@ -19,19 +24,19 @@ void loop() {
   //Serial.println(String(l) + ", " + String(m) + ", " + String(r));
   if (l > cutoff && !(m > cutoff) && !(r > cutoff)){
    Serial.println(F("Right")); 
-   servL.write(10);
+   servL.write(topL);
    servR.write(90);
   } else if (!(l > cutoff) && !(m > cutoff) && r > cutoff){
    Serial.println(F("Left"));
-   servR.write(10);
+   servR.write(topR);
    servL.write(90);
   } else if (m > cutoff || (!(l > cutoff) && !(m > cutoff) && !(r > cutoff))){
    Serial.println(F("Middle"));
-   servR.write(10);
-   servL.write(10);
+   servR.write(lowR);
+   servL.write(lowL);
   } else if (l > cutoff && !(m > cutoff) && r > cutoff){
    Serial.println(F("Fork"));
-   servL.write(10);
+   servL.write(topL);
    servR.write(90);
   }
 }
